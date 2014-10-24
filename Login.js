ba44243172aa915530d08ccf60411d2b019bd1b4
unknown <prasannakumarr@pc1577.itp.objectfrontier.com>
2014-10-24 15:24:23 +0530

    Added new fields and CSS File

--- Login.js
+++ Login.js
@@ -1,7 +1,7 @@
 var validUsers = [["prasanna","123456"],["lakshmi","123456"],["rvignesh","123456"]], formObj = document.logIn;
 function signIn()
 {
-	if(validatePassword())
+	if(validateUsername() && validatePassword())
 	{
 		var isValidUser = validateUser();
 		if(isValidUser)
@@ -14,21 +14,34 @@ function signIn()
 			return false;
 		}
 	}
+	else
+	{
+		return false;
+	}
 }
 
 function validateUsername()
 {
 	var validKeyRegex = /[a-zA-Z0-9]$/;
-	if(!(validKeyRegex.test(formObj.userName.value)))
+	var isValidUserName = true;
+	if(formObj.userName.value.length === 0)
+	{
+		alert("Username cannot be empty.");
+		formObj.userName.value = "";
+		isValidUserName = false;
+	}
+	else if(!(validKeyRegex.test(formObj.userName.value)))
 	{
 		alert("Username can only be alphanumeric. Special characters are not allowed...");
 		formObj.userName.value = "";
+		isValidUserName = false;
 	}
 	else if(formObj.userName.value.length > 8)
 	{
 		alert("Username cannot be more than 8 characters.");
-		formObj.userName.value = "";
+		isValidUserName = false;
 	}
+	return isValidUserName;
 }
 
 function validateUser()
