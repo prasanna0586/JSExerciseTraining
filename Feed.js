57083aba59044a3b23c1d6672f1e81d8cf48d2a6
prasanna0586 <prasannakumar.ramachandran@object-frontier.com>
2014-10-27 20:12:59 +0530

    Fixed delete row

--- Feed.js
+++ Feed.js
@@ -4,29 +4,16 @@ var feedRow,iconCell,feedCell,closeCell,imageElement;
 
 document.onLoad = function()
 {
-	initializeTable();	
+	var tableWithFeed = initializeTable();
+	deleteFeedData(tableWithFeed);
+	//Delete Feeds on click starts	
 }();
 function postData()
 {
 	feedArray.push(enterFeed.value);	
 	var tableWithFeed = initializeTable();
-	//Delete Feeds on click starts
-	var rows = tableWithFeed.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
-	for (i = 0; i < rows.length; i++) 
-	{
-		tableWithFeed.rows[i].cells[3].onclick = function() 
-		{
-			for (i = 0; i < rows.length; i++)
-			{
-				if(tableWithFeed.rows[i].cells[3].id === this.id)
-				{
-					feedArray.splice((feedArray.length - 1) - tableWithFeed.rows[i].rowIndex,1);
-					tableWithFeed.deleteRow(tableWithFeed.rows[i].rowIndex);
-				}
-			}
-		}
-	}
-	//Delete Feeds on click ends
+	deleteFeedData(tableWithFeed);
+	enterFeed.value = '';
 }
 
 function clearFeedData(feedContentsTable)
@@ -85,4 +72,23 @@ function initializeTable()
 		//Create close cell ends
 	}
 	return initializedTable;
+}
+
+function deleteFeedData(tableWithFeed)
+{
+	var rows = tableWithFeed.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+	for (i = 0; i < rows.length; i++) 
+	{
+		tableWithFeed.rows[i].cells[3].onclick = function() 
+		{
+			for (i = 0; i < rows.length; i++)
+			{
+				if(tableWithFeed.rows[i].cells[3].id === this.id)
+				{
+					feedArray.splice((feedArray.length - 1) - tableWithFeed.rows[i].rowIndex,1);
+					tableWithFeed.deleteRow(tableWithFeed.rows[i].rowIndex);
+				}
+			}
+		}
+	}
 }
