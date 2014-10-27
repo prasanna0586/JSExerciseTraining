ad770cc7ac45199444fab3505baa5c7c5a671bdd
lakshmiprabha <lakshmiprabha.nagarajan@object-frontier.com>
2014-10-27 14:35:21 +0530

    Fixed the delete issue

--- Feed.js
+++ Feed.js
@@ -1,16 +1,43 @@
-var feedArray = ['Existing Feed1','http://www.google.com'];
+var feedArray = ['Appa','Amma'];
 
 function postData()
 {
 	feedArray.push(enterFeed.value);
 	var table = document.getElementById("feedContent");
-	var row,cell;
+	var feedRow,feedCell,closeCell;
 	var clearedTable = clearFeedData(table);
+
 	for(var rowNum = 0; rowNum < feedArray.length; rowNum++)
 	{
-		row = clearedTable.insertRow(0);
-		cell = row.insertCell(0);
-		cell.innerHTML = feedArray[rowNum];
+		feedRow = clearedTable.insertRow(0);
+		feedRow.id = "feedRow" + (rowNum + 1);
+		
+		//Create feed cell starts
+		feedCell = feedRow.insertCell(0);
+		feedCell.id = "feedCell" + (rowNum + 1);
+		feedCell.innerHTML = feedArray[rowNum];
+		//Create feed cell ends
+		
+		//Create close cell starts
+		closeCell = feedRow.insertCell(1);
+		closeCell.id = "closeCell" + (rowNum + 1);
+		closeCell.innerHTML = "x";
+		//Create close cell ends
+	}
+	
+	var rows = clearedTable.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+	for (i = 0; i < rows.length; i++) 
+	{
+		clearedTable.rows[i].cells[1].onclick = function() 
+		{
+			for (i = 0; i < rows.length; i++)
+			{
+				if(clearedTable.rows[i].cells[1].id === this.id)
+				{
+					clearedTable.deleteRow(clearedTable.rows[i].rowIndex);
+				}
+			}
+		}
 	}
 }
 function clearFeedData(feedContentsTable)
