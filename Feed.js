20f8eba73b7df9c5275c962eecdd11588cc71bb1
prasanna0586 <prasannakumar.ramachandran@object-frontier.com>
2014-10-27 15:22:49 +0530

    Added User Icon

--- Feed.js
+++ Feed.js
@@ -4,7 +4,7 @@ function postData()
 {
 	feedArray.push(enterFeed.value);
 	var table = document.getElementById("feedContent");
-	var feedRow,feedCell,closeCell;
+	var feedRow,iconCell,feedCell,closeCell,imageElement;
 	var clearedTable = clearFeedData(table);
 
 	for(var rowNum = 0; rowNum < feedArray.length; rowNum++)
@@ -12,14 +12,22 @@ function postData()
 		feedRow = clearedTable.insertRow(0);
 		feedRow.id = "feedRow" + (rowNum + 1);
 		
+		//Create icon image cell starts
+		iconCell = feedRow.insertCell(0);
+		iconCell.id = "iconCell" + (rowNum + 1);
+		imageElement = document.createElement("img");
+		imageElement.setAttribute("src", "UserIcon.png");
+		iconCell.appendChild(imageElement);
+		//Create icon image cell ends
+		
 		//Create feed cell starts
-		feedCell = feedRow.insertCell(0);
+		feedCell = feedRow.insertCell(1);
 		feedCell.id = "feedCell" + (rowNum + 1);
 		feedCell.innerHTML = feedArray[rowNum];
 		//Create feed cell ends
 		
 		//Create close cell starts
-		closeCell = feedRow.insertCell(1);
+		closeCell = feedRow.insertCell(2);
 		closeCell.id = "closeCell" + (rowNum + 1);
 		closeCell.innerHTML = "x";
 		//Create close cell ends
@@ -28,11 +36,11 @@ function postData()
 	var rows = clearedTable.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
 	for (i = 0; i < rows.length; i++) 
 	{
-		clearedTable.rows[i].cells[1].onclick = function() 
+		clearedTable.rows[i].cells[2].onclick = function() 
 		{
 			for (i = 0; i < rows.length; i++)
 			{
-				if(clearedTable.rows[i].cells[1].id === this.id)
+				if(clearedTable.rows[i].cells[2].id === this.id)
 				{
 					clearedTable.deleteRow(clearedTable.rows[i].rowIndex);
 				}
