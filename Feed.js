0bc0148743078ee2eb4a2ef40ce5cd4ae07e47f5
prasanna0586 <prasannakumar.ramachandran@object-frontier.com>
2014-10-27 16:54:46 +0530

    Added date field

--- Feed.js
+++ Feed.js
@@ -1,16 +1,49 @@
 var feedArray = ['Appa','Amma'];
+var table = document.getElementById("feedContent");
+var feedRow,iconCell,feedCell,closeCell,imageElement;
 
+document.onLoad = function()
+{
+	initializeTable();	
+}();
 function postData()
 {
-	feedArray.push(enterFeed.value);
-	console.log("feedArray beginning " + feedArray);
-	var table = document.getElementById("feedContent");
-	var feedRow,iconCell,feedCell,closeCell,imageElement;
-	var clearedTable = clearFeedData(table);
+	feedArray.push(enterFeed.value);	
+	var tableWithFeed = initializeTable();
+	//Delete Feeds on click starts
+	var rows = tableWithFeed.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+	for (i = 0; i < rows.length; i++) 
+	{
+		tableWithFeed.rows[i].cells[3].onclick = function() 
+		{
+			for (i = 0; i < rows.length; i++)
+			{
+				if(tableWithFeed.rows[i].cells[3].id === this.id)
+				{
+					feedArray.splice((feedArray.length - 1) - tableWithFeed.rows[i].rowIndex,1);
+					tableWithFeed.deleteRow(tableWithFeed.rows[i].rowIndex);
+				}
+			}
+		}
+	}
+	//Delete Feeds on click ends
+}
 
+function clearFeedData(feedContentsTable)
+{
+	while(feedContentsTable.rows.length)
+	{
+		feedContentsTable.deleteRow(0);
+	}
+	return feedContentsTable;
+}
+
+function initializeTable()
+{
+	var initializedTable = clearFeedData(table);
 	for(var rowNum = 0; rowNum < feedArray.length; rowNum++)
 	{
-		feedRow = clearedTable.insertRow(0);
+		feedRow = initializedTable.insertRow(0);
 		feedRow.id = "feedRow" + (rowNum + 1);
 		
 		//Create icon image cell starts
@@ -27,35 +60,29 @@ function postData()
 		feedCell.innerHTML = feedArray[rowNum];
 		//Create feed cell ends
 		
+		//Create date cell starts
+		var date = new Date();
+		feedCell = feedRow.insertCell(2);
+		feedCell.id = "dateCell" + (rowNum + 1);
+		if(date.getHours() > 12)
+		{
+			hours = (date.getHours() - 12).toString();
+			amOrpm = "PM";
+		}
+		else
+		{
+			hours = date.getHours().toString();
+			amOrpm = "AM";
+		}
+		feedCell.innerHTML = date.getDate().toString() + "-" + (date.getMonth() + 1).toString() + "-" 
+		+ date.getFullYear().toString() + " " + hours + ":" + date.getMinutes().toString() + " " + amOrpm;
+		//Create date cell ends
+		
 		//Create close cell starts
-		closeCell = feedRow.insertCell(2);
+		closeCell = feedRow.insertCell(3);
 		closeCell.id = "closeCell" + (rowNum + 1);
 		closeCell.innerHTML = "x";
 		//Create close cell ends
 	}
-	
-	var rows = clearedTable.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
-	for (i = 0; i < rows.length; i++) 
-	{
-		clearedTable.rows[i].cells[2].onclick = function() 
-		{
-			for (i = 0; i < rows.length; i++)
-			{
-				if(clearedTable.rows[i].cells[2].id === this.id)
-				{
-					feedArray.splice((feedArray.length - 1) - clearedTable.rows[i].rowIndex,1);
-					clearedTable.deleteRow(clearedTable.rows[i].rowIndex);
-				}
-			}
-		}
-	}
-}
-
-function clearFeedData(feedContentsTable)
-{
-	while(feedContentsTable.rows.length)
-	{
-		feedContentsTable.deleteRow(0);
-	}
-	return feedContentsTable;
+	return initializedTable;
 }
